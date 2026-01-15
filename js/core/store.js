e31199ae566d2c94300734b49ccfae1f434ec930
pnutorres2017-byte <pnutorres2017@gmail.com>
2026-01-14 22:49:49 -0800

    Update store.js

--- js/core/store.js
+++ js/core/store.js
@@ -1,1 +1,19 @@
+import { loadState, saveState } from "../persistence/storage.js";
+
+export function initStore() {
+  let state = loadState();
+  return {
+    getState: () => state,
+    setState: (next) => {
+      state = next;
+      saveState(state);
+    },
+    update: (fn) => {
+      const next = structuredClone(state);
+      fn(next);
+      state = next;
+      saveState(state);
+    }
+  };
+}
 
