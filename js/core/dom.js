168892296c71c83e81612d7c44d23e0f7cf91763
pnutorres2017-byte <pnutorres2017@gmail.com>
2026-01-14 22:49:19 -0800

    Update dom.js

--- js/core/dom.js
+++ js/core/dom.js
@@ -1,1 +1,16 @@
+export function $(sel) {
+  const el = document.querySelector(sel);
+  if (!el) throw new Error(`Missing element: ${sel}`);
+  return el;
+}
+
+export function $all(sel) {
+  return Array.from(document.querySelectorAll(sel));
+}
+
+export function escapeHtml(s) {
+  return String(s).replace(/[&<>"']/g, c => ({
+    "&":"&amp;","<":"&lt;",">":"&gt;",'"':"&quot;","'":"&#39;"
+  }[c]));
+}
 
